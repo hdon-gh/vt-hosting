2ca3330fbeb84a4f7fda2d6766c3a6fa2270f42d
hdon-gh <89821906+hdon-gh@users.noreply.github.com>
2021-08-30 22:44:45 -0400

    test
    test

---
+++ shot_2021-08-30-2/data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-photo_6553615_dji_15_pano_43422725_0_2021830182816_photo_pano",
+      "name": "Photo_6553615_DJI_15_pano_43422725_0_2021830182816_photo_pano",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.0009427895072953163,
+        "pitch": 0.19808880128096717,
+        "fov": 1.2997806026260816
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
